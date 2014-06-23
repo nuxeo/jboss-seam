c08a651e11f7aa5caa4574bf27e9b0b98b3ca308
Anahide Tchertchian <at@nuxeo.com>
2014-06-23 12:11:17 +0200

    VEND-12: fix release script repository id

--- mvndep.sh
+++ mvndep.sh
@@ -1,8 +1,10 @@
 TARGET_REPO=https://maven-eu.nuxeo.org/nexus/content/repositories/vendor-snapshots
+TARGET_REPO_ID=vendor-snapshots
 [ "$1" == "release" ] && TARGET_REPO=https://maven-eu.nuxeo.org/nexus/content/repositories/vendor-releases
+[ "$1" == "release" ] && TARGET_REPO_ID=vendor-releases
 mvn org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file \
                    -Durl=$TARGET_REPO \
-                   -DrepositoryId=vendor-snapshots \
+                   -DrepositoryId=$TARGET_REPO_ID \
                    -Dfile=./lib/jboss-seam.jar \
                    -DpomFile=./classes/poms/core.wls.pom \
                    -Dsources=./lib/src/jboss-seam-sources.jar
