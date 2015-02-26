e1e31eef4a60e975bde356e81b701ee0381504be
Anahide Tchertchian <at@nuxeo.com>
2015-02-26 19:28:02 +0100

    VEND-22: handle build + deploy of jboss-seam-remoting too

--- mvndep.sh
+++ mvndep.sh
@@ -6,5 +6,11 @@ mvn org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file \
                    -Durl=$TARGET_REPO \
                    -DrepositoryId=$TARGET_REPO_ID \
                    -Dfile=./lib/jboss-seam.jar \
-                   -DpomFile=./classes/poms/core.wls.pom \
+                   -DpomFile=./classes/poms/core.pom \
                    -Dsources=./lib/src/jboss-seam-sources.jar
+mvn org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file \
+                   -Durl=$TARGET_REPO \
+                   -DrepositoryId=$TARGET_REPO_ID \
+                   -Dfile=./lib/jboss-seam-remoting.jar \
+                   -DpomFile=./classes/poms/remoting.pom \
+                   -Dsources=./lib/src/jboss-seam-remoting-sources.jar
